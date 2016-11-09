dfed3c4c017152b921f12f96469f19d18366a10a
GammaNu/Millicent Billette <commiter@1000i100.fr>
2016-11-09 07:45:32 +0100

    conversion des chemin environnement node en cdn pour gh-pages

--- staticApp/all.js
+++ staticApp/all.js
@@ -1,3 +1,9 @@
+requirejs.config({
+		paths: {
+			'../node_modules/d3/build/d3': '//d3js.org/d3.v4.min',
+			'../node_modules/js-yaml/dist/js-yaml': '//cdnjs.cloudflare.com/ajax/libs/js-yaml/3.6.1/js-yaml.min',
+		}
+});
 requirejs([
 		'configLoader',
 		'languageLoader',
