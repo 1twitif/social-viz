606e4d82bc94ff679ef3b25d0797dbc970164272
GammaNu/Millicent Billette <commiter@1000i100.fr>
2016-11-22 17:26:12 +0100

    switch to editor mode functionnal

--- staticApp/formLoader.js
+++ staticApp/formLoader.js
@@ -1,8 +1,9 @@
 define([
 	'./form',
 	'./smartEvents',
-	'./ymlTools'
-], (formLib, ev, ymlTools) => {
+	'./ymlTools',
+	'./configLoader'
+], (formLib, ev, ymlTools,cfg) => {
 	'use strict';
 	const on = ev.on, send = ev.send;
 
@@ -12,11 +13,18 @@ define([
 		listenerInit();
 		on('yml.ready',(e)=>(e.filename==='allData/form.yml')?send('form.template.ready',e.yml):0 );
 		ymlTools.load('allData/form.yml');
+
+		const node = document.getElementById('editionMode');
+		if (node) node.addEventListener('click', activateEditorMode);
 	}
 	function listenerInit() {
 		on('config.ready', readyToUseForm.setConfig);
 		on('data.ready', readyToUseForm.setData);
 		on('form.template.ready', readyToUseForm.setTemplate);
+	}
+	function activateEditorMode() {
+		const config = cfg.getConfig();
+		config.userMode = "edit";
 	}
 	function getForm(){
 		return readyToUseForm;
