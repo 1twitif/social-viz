ad707f6b40203f872755f5ecb9f69e48cabdcbbc
GammaNu/Millicent Billette <commiter@1000i100.fr>
2016-11-25 17:51:48 +0100

    duplication reduction (plus particulièrement dans les test)

--- staticApp/form.js
+++ staticApp/form.js
@@ -11,26 +11,39 @@ define([
 	function Form(tempateJson, data, targetNodes, id) {
 		const formObject = this;
 		let config = {};
-		this.template = tempateJson;
+		this.template = tempateJson || {};
 		this.data = data || {};
 		let entityId = id;
 		const displayAnchors = {};
 		for (let i in targetNodes) this.displayInNode(targetNodes[i]);
 		on('config.selected change', (config) => formObject.edit(config.selected));
 
-		this.setTemplate = (template) => this.template = template;
-		this.setConfig = (cfg) => config = cfg;
-		this.setData = (data) => this.data = data;
+		this.render = ()=> {
+			for(let anchor in displayAnchors) formObject.renderOn(displayAnchors[anchor]);
+		};
+		this.setTemplate = (template) => {
+			formObject.template = template;
+			formObject.render();
+		};
+		this.setConfig = (cfg) => {
+			config = cfg;
+			if(config.selected) entityId = config.selected;
+			formObject.render();
+		};
+		this.setData = (data) => {
+			formObject.data = data;
+			formObject.render();
+		};
 		this.edit = function edit(id) {
 			entityId = id;
-			// send event ou refresh render directe
+			formObject.render();
 		};
 		this.displayInNode = function displayInNode(targetNode) {
 			displayAnchors[targetNode] = targetNode;
-			this.render(targetNode);
+			this.renderOn(targetNode);
 		};
 
-		this.render = function render(targetNode) {
+		this.renderOn = (targetNode)=> {
 			targetNode.innerHTML = '';
 
 			for (let form in formObject.template) if (form !== 'enum')
@@ -39,7 +52,7 @@ define([
 					targetNode.appendChild(btn);
 					btn.addEventListener('click', () => {
 						config.selected = form + '-new';
-						render(targetNode);
+						formObject.renderOn(targetNode);
 					});
 				})(form);
 
@@ -134,7 +147,7 @@ define([
 		function getEnumFromTemplate(from) {
 			const listPath = from.split('.');
 			if (listPath[0] === 'enum') {
-				let optionList = formObject.template.enum;
+				let optionList = formObject.template.enum || {};
 				for (let i = 1; i < listPath.length; i++) {
 					optionList = optionList[listPath[i]];
 				}
@@ -169,18 +182,13 @@ define([
 		function buildDataList(entry) {
 			let dataList;
 			if (isStaticEnumRef(entry.from)) dataList = buildStaticEnumDataList(getEnumFromTemplate(entry.from));
-			else if (isValidDynamicFromRef(entry.from)) dataList = buildDynamicDataList(formObject.data[entry.from]);
-			else console.log('from non reconnu : ', entry.from);
+			else dataList = buildDynamicDataList(formObject.data[entry.from] || {});
 			dataList.setAttribute('id', dataListId(entry.from));
 			return dataList;
 		}
 
 		function isStaticEnumRef(from) {
 			return from.split('.')[0] === 'enum'
-		}
-
-		function isValidDynamicFromRef(from) {
-			return typeof formObject.data[from] !== 'undefined'
 		}
 
 		function generateId(type, title) {
--- staticApp/form.test.js
+++ staticApp/form.test.js
@@ -1,152 +1,129 @@
-define(['./form', './smartEvents'], (app,ev) => {
-	function changeInputValue(inputNode,value){
+define(['./form', './smartEvents'], (app, ev) => {
+	function changeInputValue(inputNode, value) {
 		inputNode.value = value;
-		inputNode.dispatchEvent(new Event('change',{target:inputNode,bubbles:true}));
+		inputNode.dispatchEvent(new Event('change', {target: inputNode, bubbles: true}));
 	}
+
 	describe('formulaire', () => {
-		describe('json -> objet formulaire', () => {
-			it("Construit un formulaire vide", () => {
-				const form = new app.Form();
-				expect(form instanceof app.Form).toBeTruthy();
-			});
-		});
-		describe('objet formulaire -> affichage', () => {
-			it("Affiche les formulaires disponnibles", () => {
-				const form = new app.Form({"plop":[],"plip":[]});
-				const anchor = document.createElement('div');
+		describe('selection de formulaire', () => {
+			let form, anchor;
+			beforeEach(() => {
+				form = new app.Form();
+				anchor = document.createElement('div');
 				form.displayInNode(anchor);
+			});
+			it("Affiche les formulaires disponnibles", () => {
+				form.setTemplate({"plop": [], "plip": []});
 				expect(anchor.innerHTML).toMatch('plop');
 				expect(anchor.innerHTML).toMatch('plip');
 			});
 			it("N'affiche pas enum comme un formulaire", () => {
-				const form = new app.Form({"plop":[],"enum":[]});
-				const anchor = document.createElement('div');
-				form.displayInNode(anchor);
+				form.setTemplate({"plop": [], "enum": []});
 				expect(anchor.innerHTML).toMatch('plop');
 				expect(anchor.innerHTML).not.toMatch('enum');
 			});
 			it("Charge le formulaire au click sur le bouton correspondant", () => {
-				const form = new app.Form({"plop":['myInput'],"plip":[]});
-				const anchor = document.createElement('div');
-				form.displayInNode(anchor);
+				form.setTemplate({"plop": ['myInput'], "plip": []});
 
 				expect(anchor.innerHTML).toMatch('plop');
 				expect(anchor.innerHTML).not.toMatch('myInput');
 
 				const eventSpy = new Spy();
-				ev.on('form.updated', ()=>{
-					if(anchor.querySelector('input[name="myInput"]')) eventSpy();
+				ev.on('form.updated', () => {
+					if (anchor.querySelector('input[name="myInput"]')) eventSpy();
 				});
 				ev.clickOn(anchor.querySelector('button'));
 				expect(eventSpy).toHaveBeenCalled();
 			});
 			it("Affiche directement le formulaire adequat si la config le demande", () => {
-				const form = new app.Form({"myForm":['myInput']});
-				form.setConfig({'selected':'myForm-new'});
-				const anchor = document.createElement('div');
-				form.displayInNode(anchor);
+				form.setTemplate({"myForm": ['myInput']});
+				form.setConfig({'selected': 'myForm-new'});
 				expect(anchor.querySelector('input[name="myInput"]')).toBeTruthy();
 			});
-			it("Affiche un input date", () => {
-				const form = new app.Form({"myForm":[{'myInput':{dataType:'date'}}]});
-				form.setConfig({'selected':'myForm-new'});
-				const anchor = document.createElement('div');
+		});
+		describe('affichage', () => {
+			let form, anchor;
+			beforeEach(() => {
+				form = new app.Form();
+				form.setConfig({'selected': 'myForm-new'});
+				anchor = document.createElement('div');
 				form.displayInNode(anchor);
+			});
+
+			it("Affiche un input date", () => {
+				form.setTemplate({"myForm": [{'myInput': {dataType: 'date'}}]});
 				expect(anchor.querySelector('input[type="date"][name="myInput"]')).toBeTruthy();
 			});
 			it("Affiche une textarea markdown", () => {
-				const form = new app.Form({"myForm":[{'myInput':{dataType:'markdown'}}]});
-				form.setConfig({'selected':'myForm-new'});
-				const anchor = document.createElement('div');
-				form.displayInNode(anchor);
+				form.setTemplate({"myForm": [{'myInput': {dataType: 'markdown'}}]});
 				expect(anchor.querySelector('textarea[name="myInput"]')).toBeTruthy();
 			});
 			it("gère les champs requis", () => {
-				const form = new app.Form({"myForm":[ {'myInput':{'required':true}} ]});
-				form.setConfig({'selected':'myForm-new'});
-				const anchor = document.createElement('div');
-				form.displayInNode(anchor);
+				form.setTemplate({"myForm": [{'myInput': {'required': true}}]});
 				expect(anchor.querySelector('input[name="myInput"][required]')).toBeTruthy();
 			});
 			it("gère les listes statiques", () => {
-				const form = new app.Form({
-					"myForm":[
-						{'myInput':{'from':'enum.list'}}
+				form.setTemplate({
+					"myForm": [
+						{'myInput': {'from': 'enum.list'}}
 					],
-					"enum":{'list':['one','two','tree']}
+					"enum": {'list': ['one', 'two', 'tree']}
 				});
-				form.setConfig({'selected':'myForm-new'});
-				const anchor = document.createElement('div');
-				form.displayInNode(anchor);
-
 				expect(anchor.querySelector('input[name="myInput"][list*="enum"]')).toBeTruthy();
 				expect(document.querySelector('datalist option').value).toBe('one');
 			});
 			it("gère les listes dynamique", () => {
-				const form = new app.Form({
-					"myForm":[
+				form.setTemplate({
+					"myForm": [
 						'myInput',
-						{'parent':{'from':'myForm'}}
+						{'parent': {'from': 'myForm'}}
 					]
 				});
-				form.setConfig({'selected':'myForm-new'});
-				form.setData({myForm:[
-					{id:'myForm-plop-2546',myInput:'plop',label:'Plop',parent:''}
-				]});
-				const anchor = document.createElement('div');
-				form.displayInNode(anchor);
-
+				form.setData({
+					'myForm': [
+						{'id': 'myForm-plop-2546', 'myInput': 'plop', 'label': 'Plop', 'parent': ''}
+					]
+				});
 				expect(document.getElementById('dataList-myForm').innerHTML).toMatch('Plop');
 			});
 			it("genère un id dès qu'un champ après le label est modifié", () => {
-				const form = new app.Form({
-					"myForm":['before','label','after']});
-				form.setConfig({
-					'form':{'entryDefault':{'dataType':'text'}},
-					'selected':'myForm-new'
-				});
-				form.edit('myForm-new');
-				const anchor = document.createElement('div');
-				form.displayInNode(anchor);
-
+				form.setTemplate({"myForm": ['before', 'label', 'after']});
 				const eventSpy = new Spy();
-				anchor.addEventListener('change', ()=>{
-					if(anchor.querySelector('input[name="id"][value^="myForm-safe-Label-"]'))
+				anchor.addEventListener('change', () => {
+					if (anchor.querySelector('input[name="id"][value^="myForm-safe-Label-"]'))
 						eventSpy();
 				});
-				changeInputValue(anchor.querySelector('input[name="before"]'),'osef');
-				changeInputValue(anchor.querySelector('input[name="label"]'),'!safe Labél');
+				changeInputValue(anchor.querySelector('input[name="before"]'), 'beOsef');
+				changeInputValue(anchor.querySelector('input[name="label"]'), '!safe Labél');
 				expect(eventSpy).not.toHaveBeenCalled();
-				changeInputValue(anchor.querySelector('input[name="after"]'),'osef');
+				changeInputValue(anchor.querySelector('input[name="after"]'), 'aftosef');
 				expect(eventSpy).toHaveBeenCalled();
 			});
-		});
-		it("charge les données existante quand il y en a", () => {
-			const id = 'myForm-dummy';
-			const form = new app.Form({"myForm":['before','label','after']});
-			const data = {'myForm':[{'id':id,'before':'someContent'}]};
-			form.setData(data);
-			form.setConfig({'selected':id});
-			form.edit(id);
-			const anchor = document.createElement('div');
-			form.displayInNode(anchor);
-
-			expect(anchor.querySelector('input[name="before"]').value).toEqual('someContent');
+			it("charge les données existante quand il y en a", () => {
+				const id = 'myForm-dummy';
+				form.setTemplate({"myForm": ['before', 'label', 'after']});
+				form.setData({'myForm': [{'id': id, 'before': 'someContent'}]});
+				form.edit(id);
+				expect(anchor.querySelector('input[name="before"]').value).toEqual('someContent');
+			});
 		});
 		describe('affichage / saisie -> données exportable et affichable', () => {
-			it("sauvegarde les données saisies dès qu'un id est défini", () => {
-				const form = new app.Form({"myForm":['before','label','after']});
-				form.setConfig({'selected':'myForm-new'});
-				const data = {'myForm':[]};
-				form.setData(data);
-				form.edit('myForm-new');
-				const anchor = document.createElement('div');
+			let form, anchor;
+			beforeEach(() => {
+				form = new app.Form();
+				form.setConfig({'selected': 'myForm-new'});
+				anchor = document.createElement('div');
 				form.displayInNode(anchor);
+			});
+			it("sauvegarde les données saisies dès qu'un id est défini", () => {
+				form.setTemplate({"myForm": ['before', 'label', 'after']});
+				const data = {'myForm': []};
+				form.setData(data);
 
-				changeInputValue(anchor.querySelector('input[name="before"]'),'not Yet');
-				changeInputValue(anchor.querySelector('input[name="label"]'),'!safe Labél');
-				expect(data).toEqual({'myForm':[]});
-				changeInputValue(anchor.querySelector('input[name="after"]'),'Yet !');
+				changeInputValue(anchor.querySelector('input[name="before"]'), 'not Yet');
+				changeInputValue(anchor.querySelector('input[name="label"]'), '!safe Labél');
+				expect(data).toEqual({'myForm': []});
+				changeInputValue(anchor.querySelector('input[name="after"]'), 'Yet !');
 				expect(JSON.stringify(data)).toMatch('{"myForm":\\\[{"id":"myForm-safe-Label-');
 				expect(JSON.stringify(data)).toMatch('"before":"not Yet","label":"!safe Labél","after":"Yet !"}]}');
 			});
