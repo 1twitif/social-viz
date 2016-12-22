f86d529e291da36413e94b25b5324f28262fd250
GammaNu/Millicent Billette <commiter@1000i100.fr>
2016-12-22 06:46:10 +0100

    fps is now really tested

--- staticApp/fps.js
+++ staticApp/fps.js
@@ -1,33 +1,47 @@
-define([], () => {
+define(['./smartEvents'], (ev) => {
 	'use strict';
 
-	let fpsTicks = [];
-	const lissageFPS = 5;
+	let fpsTicks, lissageFPS, fpsDisplayerNode, decimalsDisplayed;
 
+	function init(config){
+		if(!config) return ev.need('config',init);
+		fpsTicks = [];
+		fpsDisplayerNode = document.getElementById(config.fps.displayerId);
+		lissageFPS = config.fps.sampleOn;
+		decimalsDisplayed = config.fps.decimals;
+		if (fpsDisplayerNode) setInterval(fpsRefresh, s2ms(config.fps.refreshEvery));
+		ev.send('fps.ready');
+	}
 	function tick() {
 		fpsTicks.push(Date.now());
 	}
-
 	function fpsCalc() {
-		const obsolete = Date.now() - (lissageFPS * 1000);
+		const obsolete = Date.now() - (lissageFPS*1000);
 		fpsTicks = fpsTicks.filter(function (t) {
 			return t > obsolete;
 		});
-		return Math.round(fpsTicks.length / lissageFPS);
+		return fpsTicks.length / lissageFPS;
 	}
-
 	function fpsRefresh() {
-		const node = document.getElementById('fpsValue');
-		if (node) node.innerText = fpsCalc();
+		fpsDisplayerNode.innerText = round2Decimal(fpsCalc(),decimalsDisplayed);
+	}
+	function s2ms(s){
+		return Math.floor(s*1000);
+	}
+	function round2Decimal(number,decimalNumber){
+		return Math.round(Math.pow(10,decimalNumber)*number)/Math.pow(10,decimalNumber);
 	}
 
-	setInterval(fpsRefresh, 1000);
+	//TODO: mieux ranger multiTimeout dont le seul lien avec fps est le temps.
 	function multiTimeout(frequence, duration, func) {
 		for (let t = 0; t < duration; t += frequence) setTimeout(func, t);
 	}
 
+	init();
 	return {
+		init,
 		tick,
+		fpsCalc,
 		multiTimeout
 	};
 
--- staticApp/fps.test.js
+++ staticApp/fps.test.js
@@ -1,7 +1,70 @@
-define(['./fps'], (app) => {
+define(['./fps','./smartEvents'], (app,ev) => {
 	describe('fps', () => {
-		it('test nothing', () => {
-			expect(1).toBe(1);
+		describe('unit test', () => {
+			it('one tick, one fps', () => {
+				app.init({"fps":{"sampleOn":1}});
+				app.tick();
+				expect(app.fpsCalc()).toBe(1);
+			});
+			it('two ticks in 2 seconds: one fps', () => {
+				app.init({"fps":{"sampleOn":2}});
+				app.tick();
+				app.tick();
+				expect(app.fpsCalc()).toBe(1);
+			});
+			it('fps forget obsolete tick', () => {
+				jasmine.clock().install();
+				jasmine.clock().mockDate();
+				app.init({"fps":{"sampleOn":5}});
+				app.tick();
+				jasmine.clock().tick(4000);
+				expect(app.fpsCalc()).toBe(0.2);
+				jasmine.clock().tick(1000);
+				expect(app.fpsCalc()).toBe(0);
+				jasmine.clock().uninstall();
+			});
+		});
+		describe('integrated test', () => {
+			beforeEach(function() {
+				jasmine.clock().install();
+				jasmine.clock().mockDate();
+				const displayerId = "fpsValue";
+				document.body.id = displayerId;
+				app.init();
+				ev.send('config.ready',{"fps":{"sampleOn":7,"refreshEvery":3,"decimals":2,"displayerId":displayerId}});
+			});
+			afterEach(function() {
+				jasmine.clock().uninstall();
+			});
+			it('display fps rounded at 2 decimals', () => {
+				app.tick();
+				jasmine.clock().tick(2999);
+				expect(document.body.innerText).toEqual('');
+				jasmine.clock().tick(1);
+				expect(parseFloat(document.body.innerText)).toEqual(0.14);
+			});
+		});
+		describe('multiTimeout', () => {
+			let dummyCallback;
+			beforeEach(function() {
+				jasmine.clock().install();
+				jasmine.clock().mockDate();
+				dummyCallback = new Spy();
+			});
+			afterEach(function() {
+				jasmine.clock().uninstall();
+			});
+			it('call it callback regulary until limit', () => {
+				app.multiTimeout(10, 50, dummyCallback);
+				jasmine.clock().tick(1);
+				expect(dummyCallback.calls.count()).toBe(1);
+				jasmine.clock().tick(9);
+				expect(dummyCallback.calls.count()).toBe(2);
+				jasmine.clock().tick(40);
+				expect(dummyCallback.calls.count()).toBe(5);
+				jasmine.clock().tick(1000);
+				expect(dummyCallback.calls.count()).toBe(5);
+			});
 		});
 	});
 });
