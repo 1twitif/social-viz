8b2588771c6f62261b14b9f362d0e199f173b563
GammaNu - Millicent Billette <git@1000i100.fr>
2017-01-11 21:10:38 +0100

    refacto tradRenderer

--- staticApp/tradRenderer.js
+++ staticApp/tradRenderer.js
@@ -23,27 +23,32 @@ define(['./smartEvents', './structManipulation'], (ev,struct) => {
 		else return translateMe;
 	}
 
-	//FIXME: rendre langBrutalSwith de nouveau fonctionnel
-	function refreshTrad() { // ex langBrutalSwitch
+	function refreshTrad() {
 		let allKeys = struct.merge(tradData, oldTradData); // allKeys handle partial traductions.
 		for (let key in allKeys) if (!oldTradData[key]) allKeys[key] = key;
 		let walker = document.createTreeWalker(document, NodeFilter.SHOW_ALL, null, false);
 		let node;
 		while (node = walker.nextNode()) {
-			if (node.data) {
-				let text = node.data;
-				for (let key in allKeys) text = text.replace(allKeys[key], t(key));
-				node.data = text;
-			}
-			if (node.attributes) {
-				for (let i in node.attributes) {
-					let attr = node.attributes[i];
+			refreshTextTrad(node, allKeys);
+			refreshAttributesTrad(node, allKeys);
+		}
+	}
+	function refreshTextTrad(node, allKeys) {
+		if (node.data) {
+			let text = node.data;
+			for (let key in allKeys) text = text.replace(allKeys[key], t(key));
+			node.data = text;
+		}
+	}
+	function refreshAttributesTrad(node, allKeys) {
+		if (node.attributes) {
+			for (let i in node.attributes) {
+				let attr = node.attributes[i];
 
-					if(attr.name === "class" || attr.name === "id" || attr.name === "type") continue;
-					let text = attr.value;
-					if (text) for (let key in allKeys) text = text.replace(allKeys[key], t(key));
-					if (attr.value !== text) attr.value = text;
-				}
+				if (attr.name === "class" || attr.name === "id" || attr.name === "type") continue;
+				let text = attr.value;
+				if (text) for (let key in allKeys) text = text.replace(allKeys[key], t(key));
+				if (attr.value !== text) attr.value = text;
 			}
 		}
 	}
