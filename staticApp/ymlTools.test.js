399495d7fc5faf629fa5fc42c87f7138cbe8c96b
GammaNu/Millicent Billette <commiter@1000i100.fr>
2017-02-13 06:07:44 +0100

    fix yaml exception in chrome

--- staticApp/ymlTools.test.js
+++ staticApp/ymlTools.test.js
@@ -7,7 +7,7 @@ define(['./ymlTools', './smartEvents'], (app, ev) => {
 		afterEach(ev.reset);
 		it('convert ymlText to jsObject', () => {
 			const dummyCallback = new Spy();
-			const inputData = {"filename": "osef", "fileContent": "plop: {id: 0, label: Publication}\n\n"};
+			const inputData = {"filename": "osef", "fileContent": "plop:\n  id: 0\n  label: Publication"};
 			const expected = {'filename': 'osef', 'yml': {'plop': {'id': 0, 'label': 'Publication'}}};
 			ev.on('yml.ready', dummyCallback);
 			app.convert(inputData);
@@ -49,13 +49,13 @@ define(['./ymlTools', './smartEvents'], (app, ev) => {
 			const backup = window.fetch;
 			window.fetch = ()=>{return{then:()=>{return{then:()=>'osef'}}}};
 
-			app.loadMerge(['firstFile.uglyestMock','secondFile.uglyestMock'],(result)=>{
+			app.loadMerge(['firstFile','secondFile'],(result)=>{
 				expect(result.first).toBe(true);
 				expect(result.second).toBe("overwritten");
 				done();
 			});
-			ev.send('file.ready', {'filename': 'secondFile.uglyestMock', 'fileContent': "second: overwritten\n\n"});
-			ev.send('file.ready', {'filename': 'firstFile.uglyestMock', 'fileContent': "second: initial\nfirst: true\n\n"});
+			ev.send('file.ready', {'filename': 'secondFile', 'fileContent': "second: overwritten"});
+			ev.send('file.ready', {'filename': 'firstFile', 'fileContent': "second: initial\nfirst: true"});
 
 			window.fetch = backup;
 		});
